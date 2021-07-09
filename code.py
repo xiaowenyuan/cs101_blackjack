268c39dc6486bd39a2480f448261c2db889ae85d
Xiu <xiaowenyuan@users.noreply.github.com>
2021-07-09 22:25:24 +0800

    Added basic logic for AI dealer to play against the user.

--- code.py
+++ code.py
@@ -1,10 +1,11 @@
 import random
 class Card:
-    def __init__(self, id, name, suit, value):
+    def __init__(self, id, name, suit, value, visibility = True):
         self.id = id
         self.name = name + " " + suit.title()
         self.suit = suit.title()
         self.value = value
+        self.visibility = visibility
         if self.id == 11:
             self.name = "Jack of " + self.suit 
             self.value = 10
@@ -19,7 +20,10 @@ class Card:
             self.value = 11
     
     def __repr__(self):
-        return self.name
+        if self.visibility == False:
+            return "Hidden"
+        else:  
+            return self.name
 
 def generate_cards(suit): 
     cards = []
@@ -43,9 +47,10 @@ current_deck = shuffle_cards()
 
 user_hand = []
 
+ai_hand = []
+
 def deal_card(hand, deck):
     dealt_card = deck.pop(0)
-    print("You got",  dealt_card)
     hand.append(dealt_card)
 
 def card_count(hand):
@@ -62,8 +67,23 @@ def card_count(hand):
                     break
         else:
             break
-    print("Current sum: ", count_card_sum)
     return count_card_sum
+
+def ai_decision(hand, deck):
+    if len(hand) == 1:
+        deal_card(hand,deck)
+        hand[1].visibility = False
+    elif card_count(hand) < 17:
+        deal_card(hand, deck)
+    else:
+        print("The House stands.")
+
+def reveal_hole(hand):
+    for card in hand:
+        if card.visibility == False:
+            card.visibility = True
+    string_to_return = "The House reveals its hand: " + str(hand)
+    return string_to_return
 
 game_running = True
 
@@ -73,20 +93,56 @@ while game_running:
     if user_hand == []:
         user_input = input("Type \'Hit me\' to be dealt a card.")
     else:
-        user_input = input("Type \'Hit me\' to be dealt a card or \'Hold\' to hold.")
+        user_input = input("Type \'Hit me\' to be dealt a card or \'Stand\' to stand.")
     if user_input.lower() == "hit me":
         deal_card(user_hand, current_deck)
-        print("You have: ", user_hand)
+        print("You have:", user_hand)
+        ai_decision(ai_hand, current_deck)
+        print("The House has:", ai_hand)
         current_hand_sum = card_count(user_hand)
+        ai_hand_sum = card_count(ai_hand)
+        if ai_hand_sum > 21 and current_hand_sum > 21:
+            print(reveal_hole(ai_hand))
+            print("Everyone lost!")
+            game_running = False
+        if ai_hand_sum == 21 and current_hand_sum == 21:
+            print(reveal_hole(ai_hand))
+            print("Blackjack! Everyone wins!")
+            game_running = False
         if current_hand_sum == 21:
             print("Blackjack!")
             print("You win! Thank you for playing. Goodbye.")
             game_running = False
-        elif current_hand_sum > 21:
-            print("Busted!")
+        if current_hand_sum > 21:
+            print(reveal_hole(ai_hand))
+            print("Busted! The House wins.")
             print("Thank you for playing. Goodbye.")
             game_running = False
-    if user_input.lower() == "hold":
+        if ai_hand_sum == 21:
+            print(reveal_hole(ai_hand))
+            print("The House has Blackjack. Thank you for playing. Goodbye.")
+            game_running = False
+        if ai_hand_sum > 21 and current_hand_sum < 21:
+            print(reveal_hole(ai_hand))
+            print("The House loses. Thank you for playing. Goodbye.")
+            game_running = False
+        if ai_hand_sum > 21 and current_hand_sum > 21:
+            print(reveal_hole(ai_hand))
+            print("Everyone lost!")
+            game_running = False
+    if user_input.lower() == "stand":
         print("You have: ", user_hand)
-        print("Thank you for playing. Goodbye.")
-        game_running = False
+        ai_decision(ai_hand, current_deck)
+        if ai_hand_sum < 21 and ai_hand_sum > current_hand_sum:
+            print(reveal_hole(ai_hand))
+            print("The House wins. Thank you for playing. Goodbye.")
+            game_running = False
+        if ai_hand_sum < 21 and ai_hand_sum < current_hand_sum:
+            print(reveal_hole(ai_hand))
+            print("You win. Thank you for playing. Goodbye.")
+            game_running = False
+        if ai_hand_sum == current_hand_sum:
+            print(reveal_hole(ai_hand))
+            print("It's a draw. Everyone wins!") 
+            game_running = False
+        
